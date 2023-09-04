7b1d9ae9cc6e5b0b023f59b37be0a03ca8dcb7ac
Thomas G <thomas.ngataly@gmail.com>
2023-09-04 18:47:56 +0200

    Initial Commit

---
+++ AmericanDream/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AmericanDream
+//
+//  Created by Thomas Gataly on 04/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
